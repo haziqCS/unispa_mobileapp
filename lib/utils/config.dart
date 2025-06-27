e3b0d2dba21d7f08f19d625e42414cacb562bd30
1Haziq <haziqhusni59@gmail.com>
2025-06-28 00:49:53 +0800

    Initial Configurations, Working on welcome and login page

---
+++ lib/utils/config.dart
@@ -0,0 +1,44 @@
+//Set constant configuration here
+import 'package:flutter/material.dart';
+
+class Config {
+  static MediaQueryData? mediaQueryData;
+  static double? screenWidth;
+  static double? screenHeight;
+
+  //Width and Height initialization
+  void init(BuildContext context) {
+    mediaQueryData = MediaQuery.of(context);
+    screenWidth = mediaQueryData!.size.width;
+    screenHeight = mediaQueryData!.size.height;
+  }
+
+  static get widthSize {
+    return screenWidth;
+  }
+
+  static get heightSize {
+    return screenHeight;
+  }
+
+  //Define spacing height
+  static const spaceSmall = SizedBox(height: 25);
+  static final spaceMedium = SizedBox(height: screenHeight! * 0.05);
+  static final spaceBig = SizedBox(height: screenHeight! * 0.08);
+
+  //Textform Field Border
+  static const outlinedBorder = OutlineInputBorder(
+    borderRadius: BorderRadius.all(Radius.circular(8)),
+  );
+
+  static const focusBorder = OutlineInputBorder(
+    borderRadius: BorderRadius.all(Radius.circular(8)),
+    borderSide: BorderSide(color: Colors.teal),
+  );
+  static const errorBorder = OutlineInputBorder(
+    borderRadius: BorderRadius.all(Radius.circular(8)),
+    borderSide: BorderSide(color: Colors.orange),
+  );
+
+  static const primaryColor = Colors.greenAccent;
+}
---
+++ lib/utils/text.dart
@@ -0,0 +1,13 @@
+//This is pre-define text for the app
+
+class AppText {
+  static final enText = {
+    'welcome_text' : 'Welcome',
+    'signIn_text' : 'Sign in to your account',
+    'registered_text' : 'Already have an account?',
+    'register_text' : 'You can easily sign up, and book your treatment with this app',
+    'signUp_text' : 'Don\'t have an account?',
+    'social_login' : 'Or continue with social account',
+    'forgot_password' : 'Forgot yout password?',
+  };
+}
