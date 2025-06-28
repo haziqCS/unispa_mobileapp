8c25c90074fa1e3c589f8f8f6beaf6a7c7b7e593
1Haziq <haziqhusni59@gmail.com>
2025-06-28 16:44:20 +0800

    Addedd login page, Noted bugs: keyboard don't show up when entering email, social buttons overflowed when the keyboard is visible

--- lib/main.dart
+++ lib/main.dart
@@ -51,28 +51,9 @@ class MyApp extends StatelessWidget {
         //This is for main layout after login
         'main': (context) => const MainLayout(),
       },
-      home: const MyHomePage(title: 'UniSpa App'),
+      //home: const MyHomePage(title: 'UniSpa App'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
 
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-    );
-  }
-}
--- lib/screens/auth_page.dart
+++ lib/screens/auth_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:unispa_mobileapp/components/login_form.dart';
+import 'package:unispa_mobileapp/components/social_button.dart';
+import 'package:unispa_mobileapp/utils/config.dart';
+import 'package:unispa_mobileapp/utils/text.dart';
 
 class AuthPage extends StatefulWidget {
   const AuthPage({Key? key}) : super(key: key);
@@ -10,9 +14,93 @@ class AuthPage extends StatefulWidget {
 class _AuthPageState extends State<AuthPage> {
   @override
   Widget build(BuildContext context) {
+    Config().init(context);
+    //Login page text field
     return Scaffold(
-      body: Center(
-        child: Text('Auth Page'),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+        child: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                AppText.enText['welcome_text']!,
+                style: const TextStyle(
+                  fontSize: 36,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Config.spaceSmall,
+              Text(
+                AppText.enText['signIn_text']!,
+                style: const TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Config.spaceSmall,
+              //Login Components
+              const LoginForm(),
+              Config.spaceSmall,
+              Center(
+                child: TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    AppText.enText['forgot_password']!,
+                    style: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+              ),
+              //Social Button Sign In
+              const Spacer(),
+              Center(
+                child: Text(
+                  AppText.enText['social_login']!,
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.normal,
+                    color: Colors.grey.shade500,
+                  ),
+                ),
+              ),
+              Config.spaceSmall,
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: const <Widget>[
+                  SocialButton(social: 'google'),
+                  SocialButton(social: 'facebook'),
+                ],
+              ),
+              Config.spaceSmall,
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    AppText.enText['signUp_text']!,
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.normal,
+                      color: Colors.grey.shade500,
+                    ),
+                  ),
+                  const Text(
+                  'Sign Up',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
+                ),
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
